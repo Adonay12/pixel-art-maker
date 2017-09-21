474c5ce9c5b1c2d107910af7f3ebff306cad75f9
Adonay Lebeneh <adonayamsalu@gmail.com>
2017-09-21 10:46:09 -0700

    only boxes are working; no colors added

---
+++ index.js
@@ -0,0 +1,20 @@
+var width = screen.width;
+var height = screen.height;
+//var body = document.querySelector('body');
+var section = 'div0'
+var boxHolder = document.getElementById('boxHolder')
+
+  for(var i = 0; i <= 1049; i++){
+    let newBox = document.createElement('div');
+    boxHolder.append(newBox);
+  }
+var colorsB = document.getElementById('color');
+
+  
+  for(var i = 0; i <=20; i++){
+    let newColorBox = document.createElement('div');
+    colorsB.append(newColorBox);
+  }   
+
+
+  
