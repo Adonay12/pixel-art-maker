da1c49fdaf898c6fc8e92b355641d55950d595aa
Adonay Lebeneh <adonayamsalu@gmail.com>
2017-09-21 12:20:21 -0700

    now changing colors

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 var width = screen.width/20.8888888885;
 var height = screen.height/20.07;
-var boxes = Number(width*height);
+
+var boxes = Math.floor(width*height);
+console.log(boxes);
 var boxHolder = document.getElementById('boxHolder')
 
-  for(var i = 0; i <=boxes ; i++){
+  for(var i = 0; i <=boxes - 1; i++){
     let newBox = document.createElement('div');
     boxHolder.append(newBox);
   }
+var colorChoosed = 'white';
+var containerColors = document.getElementsByClassName('col');
+
+for(var i = 0; i < containerColors.length; i++){
+
+containerColors[i].addEventListener("click", function( event ) {
+  if(event.target.nodeName === "DIV"){
+  colorChoosed =  event.target.id;
+  }
+})
+}
+
 boxHolder.addEventListener("click", function( event ) {
   if(event.target.nodeName === "DIV"){
-  event.target.style.backgroundColor = "black"
+  event.target.style.backgroundColor = colorChoosed;
   }
 })
